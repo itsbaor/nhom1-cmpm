d4621fc47d5de65b6feaef8b88bc44b6128b2bb0
dinh nam <nguyendinhnama1k51dta@gmail.com>
2025-02-21 11:29:52 +0700

    update search

--- server/src/main/java/com/nhom1/socialmedia/controller/SearchController.java
+++ server/src/main/java/com/nhom1/socialmedia/controller/SearchController.java
@@ -1,11 +1,27 @@
 package com.nhom1.socialmedia.controller;
 
+import com.nhom1.socialmedia.model.Posts;
+import com.nhom1.socialmedia.repository.SearchRepository;
+import com.nhom1.socialmedia.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
 public class SearchController {
+    @Autowired
+    private SearchService searchService;
+
+    @GetMapping("/search")
+    public List<Posts> searchPosts(@RequestParam String keyword) {
+        return searchService.searchPosts(keyword);
+
+    }
 
 }
 
