d1ee48eed341a6b8485ef7f499245bb972bc0a28
dinh nam <nguyendinhnama1k51dta@gmail.com>
2025-02-24 11:18:44 +0700

    up file SavePortService to project

---
+++ server/src/main/java/com/nhom1/socialmedia/repository/SavePostRepository.java
@@ -0,0 +1,8 @@
+import com.nhom1.socialmedia.model.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SavePostRepository extends JpaRepository<Posts, Long> {
+
+}
