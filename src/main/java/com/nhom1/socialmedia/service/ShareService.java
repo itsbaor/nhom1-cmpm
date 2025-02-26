dab11a3cbc203a0d6342b610eb312a15438b03f4
dinh nam <nguyendinhnama1k51dta@gmail.com>
2025-02-26 22:02:09 +0700

    update file shareservice

--- src/main/java/com/nhom1/socialmedia/service/ShareService.java
+++ src/main/java/com/nhom1/socialmedia/service/ShareService.java
@@ -1,10 +1,13 @@
 package com.nhom1.socialmedia.service;
 
 import com.nhom1.socialmedia.model.Posts;
+import com.nhom1.socialmedia.model.User;
 import com.nhom1.socialmedia.repository.ShareRepository;
+import com.nhom1.socialmedia.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -12,8 +15,22 @@ public class ShareService {
     @Autowired
     private ShareRepository shareRepository;
 
-    //lay id theo bai viet
-    public Optional<Posts> getPostById(Long id){
-return shareRepository.findById(id);
+    @Autowired
+    private UserRepository userRepository;
+    //chia sẻ bài viết
+    public void sharePost(long postId, long userId) {
+        Posts post = shareRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("Bài đăng không tồn tại"));
+        User user = userRepository.findById(userId)
+                .orElseThrow(()  -> new RuntimeException("Người dùng không tồn tại"));
+        //thêm người dùng vào danh sách sharuser
+        post.getShareUser().add(user);
+        shareRepository.save(post);
+    }
+    //lấy danh sách người dùng chia sẻ bài viết
+    public List<User> getShareUsersByPostId(Long postId) {
+        Posts post = shareRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("Post not found"));
+        return post.getShareUser();
     }
 }
