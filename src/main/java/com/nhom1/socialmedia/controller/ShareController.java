b2c6df7af48e3f77dd559b5979e0d95b523b3690
dinh nam <nguyendinhnama1k51dta@gmail.com>
2025-02-27 18:40:57 +0700

    update shareController

--- src/main/java/com/nhom1/socialmedia/controller/ShareController.java
+++ src/main/java/com/nhom1/socialmedia/controller/ShareController.java
@@ -1,31 +1,31 @@
 package com.nhom1.socialmedia.controller;
 
 import com.nhom1.socialmedia.model.Posts;
+import com.nhom1.socialmedia.model.User;
 import com.nhom1.socialmedia.service.ShareService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/shares")
+@RequestMapping("share")
 public class ShareController {
 
     @Autowired
     private ShareService shareService;
-
+    //lay danh sach nguoi dung chia se bai viet
+    @GetMapping("/{postId}/share-users")
+    public ResponseEntity<List<User>> getShareUsersByPostId(@PathVariable long postId) {
+        List<User> shareUsers = shareService.getShareUsersByPostId(postId);
+        return ResponseEntity.ok(shareUsers);
+    }
     //chia se bai viet
-    @GetMapping("/{id}/share")
-    public String sharePost(@PathVariable Long id){
-        Optional<Posts> share = shareService.getPostById(id);
-        if(share.isPresent()){
-            String shareUrl = "http://localhost:8080/api/shares" + id;
-            return "Chia sẻ bài viết: " + shareUrl;
-        } else {
-            return "Bài viết không tồn tại";
-        }
+    @PostMapping("/{postId}/share/{userId}")
+    public ResponseEntity<?> sharePost(@PathVariable long postId, @PathVariable long userId) {
+        shareService.sharePost(postId, userId);
+        return ResponseEntity.ok().build();
     }
 }
